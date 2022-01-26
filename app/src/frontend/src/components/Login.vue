78f5fbf8cc9e4d98b084f327ba8b645fd17ef4dd
Mher <charachianmher@live.com>
2022-01-26 15:41:39 -0500

    1- created new views for login and registration 2- moved the item card code out of home and into its own component page so it is reusable 3- updated the header component to have an account button and a cart button

---
+++ app/src/frontend/src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="login" lang="scss">
+    
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      msg: null,
+    };
+  },
+  mounted() {
+    axios.get("/sanctum/csrf-cookie").then(() => {
+      axios.get("/login").then((response) => (this.msg = response.data));
+    });
+  },
+};
+</script>
+
+<style scoped>
+.md-toolbar{
+  justify-content: center;
+}
+.md-icon{
+  margin: auto 0.5em;
+}
+</style>
--- app/src/frontend/src/views/Home.vue
+++ app/src/frontend/src/views/Home.vue
@@ -1,36 +1,17 @@
 <script>
+import ItemCard from '../components/ItemCard.vue';
+
 export default {
   name: "Home",
+  components: {
+    ItemCard,
+  },
 };
 </script>
 
 <template>
   <div id="home" lang="scss" class="md-layout md-gutter md-alignment-center">
-    <div
-      class="
-        md-layout-item md-size-30
-      "
-    >
-      <md-card>
-        <md-card-header>
-          <div class="md-title">Item</div>
-          <md-card-media>
-            <img src="" alt="Item-Picture">
-          </md-card-media>
-        </md-card-header>
-
-        <md-card-content>
-          <p>
-            Description of item
-          </p>
-        </md-card-content>
-
-        <md-card-actions>
-          <md-button>Add To Card</md-button>
-          <md-button>Buy Now</md-button>
-        </md-card-actions>
-      </md-card>
-    </div>
+    <ItemCard/>
   </div>
 </template>
 
