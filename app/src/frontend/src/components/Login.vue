0269169f9a69a739f459694513a288cefd6f42dc
Mher Charachian <charachianmher@live.com>
2022-01-27 09:27:50 -0500

    1- moved routing and router related logic to main.js 2- started login form design

--- app/src/frontend/src/components/Login.vue
+++ app/src/frontend/src/components/Login.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="login" lang="scss">
-    
+    <form
+      novalidate
+      id="login-form"
+      ref="loginForm"
+      class="md-layout md-alignment-center-center"
+    >
+      <md-card class="md-layout-item md-size-40">
+        <md-card-header>
+          <div class="md-title">Login</div>
+        </md-card-header>
+
+        <md-card-content>
+          <div class="md-layout-item">
+            <!-- <md-field :class="getValidationClass('email')">
+
+            </md-field> -->
+          </div>
+        </md-card-content>
+      </md-card>
+    </form>
   </div>
 </template>
 
@@ -23,10 +42,16 @@ export default {
 </script>
 
 <style scoped>
-.md-toolbar{
+.md-toolbar {
   justify-content: center;
 }
-.md-icon{
+.md-icon {
   margin: auto 0.5em;
+}
+
+#login-form {
+  position: fixed;
+  top: 50%;
+  min-width: 100%;
 }
 </style>
--- app/src/frontend/src/main.js
+++ app/src/frontend/src/main.js
@@ -1,36 +1,53 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Router from "vue-router";
+
+import { Home, Admin } from "./views";
+import { Login } from "./components";
+
 import { MdToolbar, MdButton, MdIcon, MdCard } from "vue-material/dist/components";
+
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
 import "./assets/styles/theme.scss";
-import home from "./router/index";
-import admin from "./router/admin";
 
 Vue.config.productionTip = false;
 
 const host = window.location.host;
 const subdomain = host.split(".")[0];
 
-const router = () => {
+const routes = () => {
   let routes;
   if (subdomain === "store") {
-    routes = home;
+    routes = [
+      { path: "/", component: Home },
+      { path: "/login", component: Login },
+    ]
   } else if (subdomain === "admin") {
-    routes = admin;
+    routes = [
+      { path: "/", component: Admin },
+    ]
   } else {
-    routes = home;
+    routes = []
   }
   return routes;
 };
 
+
+const router = new Router({
+  mode: "history",
+  routes: routes()
+})
+
+
+Vue.use(Router);
 Vue.use(MdToolbar);
 Vue.use(MdButton);
 Vue.use(MdIcon);
 Vue.use(MdCard);
 
 new Vue({
-  router: router(),
+  router,
   render: (h) => h(App),
   components: {
     App,
