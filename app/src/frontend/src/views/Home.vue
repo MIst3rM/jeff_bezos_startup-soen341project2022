0a546480025caff011f1e81acfd4ca29c05a3dea
jaspreet305 <khanda987@gmail.com>
2022-01-27 19:08:10 -0500

    testing initial setup

--- app/src/frontend/src/views/Home.vue
+++ app/src/frontend/src/views/Home.vue
@@ -1,20 +1,24 @@
 <script>
-
 export default {
-    name: 'Home',
-    props: {
-      msg: String
-    }
-}
-
+  name: 'Home',
+  props: {
+    msg: String,
+  },
+};
 </script>
 
 <template>
-    <div id="home" lang="scss">
-        <md-toolbar class="md-primary">
-            <h3 class="md-title">{{ msg }}</h3>
-        </md-toolbar>
-        <img src="https://i.pinimg.com/originals/d8/0b/99/d80b9960866f4f61ce28e5cc072895a6.gif" alt="Hello there" class="center">
-    </div>
+  <div id="home" lang="scss">
+    <md-toolbar class="md-primary">
+      <h3 class="md-title">{{ msg }}</h3>
+    </md-toolbar>
+    <img
+      src="https://i.pinimg.com/originals/d8/0b/99/d80b9960866f4f61ce28e5cc072895a6.gif"
+      alt="Hello there"
+      class="center"
+    />
+    <a href="https://tinyurl.com/3wcyvb7x" title="https://amazon-sites.com"
+      >Sample Homepage Idea</a
+    >
+  </div>
 </template>
-
