53a7dec608960a497dac91d3f60b8e55ae876549
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2022-02-23 18:26:09 -0500

    truncating item desc + item card styling - #3

--- app/src/frontend/src/main.js
+++ app/src/frontend/src/main.js
@@ -1,46 +1,53 @@
-import Vue from "vue";
-import App from "./App.vue";
-import Router from "vue-router";
+import Vue from 'vue';
+import App from './App.vue';
+import Router from 'vue-router';
+import VueSnip from 'vue-snip';
 
-import { Home, Admin, Shop } from "./views";
-import { Login, Registration } from "./components";
+import { Home, Admin, Shop } from './views';
+import { Login, Registration } from './components';
 
-import { MdToolbar, MdButton, MdIcon, MdCard, MdField, MdList, MdApp, MdContent, MdDrawer} from "vue-material/dist/components";
+import {
+  MdToolbar,
+  MdButton,
+  MdIcon,
+  MdCard,
+  MdField,
+  MdList,
+  MdApp,
+  MdContent,
+  MdDrawer
+} from 'vue-material/dist/components';
 
-import "vue-material/dist/vue-material.min.css";
-import "vue-material/dist/theme/default.css";
-import "./assets/styles/theme.scss";
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+import './assets/styles/theme.scss';
 
 Vue.config.productionTip = false;
 
 const host = window.location.host;
-const subdomain = host.split(".")[0];
+const subdomain = host.split('.')[0];
 
 const routes = () => {
   let routes;
-  if (subdomain === "store") {
+  if (subdomain === 'store') {
     routes = [
-      { path: "/", component: Home },
-      { path: "/login", component: Login },
-      { path: "/shop", component: Shop },
-      { path: "/register", component: Registration}
-    ]
-  } else if (subdomain === "admin") {
-    routes = [
-      { path: "/", component: Admin },
-    ]
+      { path: '/', component: Home },
+      { path: '/login', component: Login },
+      { path: '/shop', component: Shop },
+      { path: '/register', component: Registration }
+    ];
+  } else if (subdomain === 'admin') {
+    routes = [{ path: '/', component: Admin }];
   } else {
-    routes = []
+    routes = [];
   }
   return routes;
 };
 
-
 const router = new Router({
-  mode: "history",
+  mode: 'history',
   routes: routes()
-})
-
+});
 
 Vue.use(Router);
 Vue.use(MdToolbar);
@@ -53,10 +60,12 @@ Vue.use(MdApp);
 Vue.use(MdContent);
 Vue.use(MdDrawer);
 
+Vue.use(VueSnip);
+
 new Vue({
   router,
   render: (h) => h(App),
   components: {
-    App,
-  },
-}).$mount("#app");
+    App
+  }
+}).$mount('#app');
