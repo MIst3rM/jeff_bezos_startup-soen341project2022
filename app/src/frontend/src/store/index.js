fd88fda697c9447d3fe3a7e6db50a45823339cd1
“noahassayag” <assayagnoah@gmail.com>
2022-04-01 10:37:52 -0400

    Created mutationtypes and added mutations #4

--- app/src/frontend/src/store/index.js
+++ app/src/frontend/src/store/index.js
@@ -2,8 +2,26 @@ import Vuex from "vuex";
 import Vue from "vue";
 import createPersistedState from "vuex-persistedstate";
 import auth from "./modules/auth";
+import * as types from './mutation-types';
 
 Vue.use(Vuex);
+
+// mutations
+const mutations = {
+
+  [types.ADD_TO_CART] (state, { id }) {
+      const record = state.added.find(p => p.id === id)
+
+      if (!record) {
+        state.added.push({
+          id,
+          quantity: 1
+        })
+      } else {
+        record.quantity++
+      }
+    }
+}
 
 export default new Vuex.Store({
   modules: {
@@ -15,4 +33,8 @@ export default new Vuex.Store({
       storage: window.sessionStorage,
     }),
   ],
+  state,
+  getters,
+  actions,
+  mutations
 });
