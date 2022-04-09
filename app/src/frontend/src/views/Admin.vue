ec7e36b2e05314c17ca4dfce5073b8dd2d344e3d
Ryan Kim <rck2001@hotmail.com>
2022-04-09 15:24:27 -0400

    updated admin vue with tabs
    personal info tab, sales tab, listed items tab

--- app/src/frontend/src/views/Admin.vue
+++ app/src/frontend/src/views/Admin.vue
@@ -1,20 +1,41 @@
 <template>
-  <div>
+  <div id="adminPageContainer">
     <AdminHeader />
-    <ItemsList />
+    <md-app md-waterfall md-mode="fixed">
+      <md-app-drawer class="md-elevation-5" md-permanent="full">
+        <md-tabs md-sync-route>
+          <md-tab class="tab-btn" md-label="Personal Information"></md-tab>
+          <md-tab class="tab-btn" md-label="Sales">Sales</md-tab>
+          <md-tab
+            class="tab-btn"
+            :to="{ name: 'listed' }"
+            md-label="Listed Items"
+            >Listed</md-tab
+          >
+        </md-tabs>
+      </md-app-drawer>
+      <md-app-content>
+        <md-empty-state
+          v-if="$route.matched.length === 1"
+          md-icon="manage_accounts"
+          md-label="Choose an option to get started"
+          md-description="Click on one of the tabs on the left."
+        >
+        </md-empty-state>
+        <router-view />
+      </md-app-content>
+    </md-app>
   </div>
 </template>
 
 
 <script>
-import { AddItem, AdminHeader, ItemsList } from "../components";
+import { AdminHeader } from "../components";
 
 export default {
   name: "Admin",
   components: {
-    AddItem,
     AdminHeader,
-    ItemsList,
   },
   computed: {
     login() {
@@ -27,5 +48,28 @@ export default {
 <style lang="scss" scoped>
 .md-layout {
   width: 100%;
+}
+
+.md-app {
+  margin: 1px;
+}
+
+.md-app-content {
+  border: none;
+}
+
+.md-drawer {
+  width: min-content;
+  max-width: calc(100vw - 125px);
+  height: 96vh;
+}
+
+.md-tabs :nth-child(1) {
+  flex-direction: column;
+  width: fit-content;
+}
+
+#adminPageContainer {
+  height: 100vh;
 }
 </style>
