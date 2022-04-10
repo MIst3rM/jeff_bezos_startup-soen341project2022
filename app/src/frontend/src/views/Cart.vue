6670d7c74790f884c422022beef799f9db511721
Alex Santelli <santellialex@gmail.com>
2022-04-09 21:28:58 -0400

    Added Cart.vue Frontend aspect

--- app/src/frontend/src/views/Cart.vue
+++ app/src/frontend/src/views/Cart.vue
@@ -1,87 +1,221 @@
 <template>
   <div class="cart">
-    <h1 class="title">Your Cart</h1>
-    <p class ="cart-message" v-show="!products.length">
-      <i>Your cart is empty! </i>
-      <router-link to="/"> Go shopping</router-link>
-    </p>
-    <table class="table" v-show="products.length">
-      <thead>
-        <tr>
-          <td>Name</td>
-          <td>Price</td>
-          <td>Quantity</td>
-        </tr>
-      </thead>
-      <tbody>
-            <tr v-for-key="p in products">
-            <td>{{ p.name }}</td>
-            <td>${{ p.price }}</td>
-            <td>{{ p.quantity }}</td>
-          </tr>
-          <tr>
-            <td><b>Total:</b></td>
-            <td></td>
-            <td><b>${{ total }}</b></td>
-          </tr>
-      </tbody>
-
-    </table>
-    <span id="login-button">
-              <md-button
-                v-show="products.length"
-                id="login-color"
-                type="submit"
-                class="md-primary"
-                :disabled="sending"
-                @click='checkout'>
-                Checkout
-                </md-button
-              >
-            </span>
+    <md-empty-state
+      v-if="getItems.length == 0"
+      md-icon="shopping_basket"
+      md-label="Your cart is empty"
+      md-description="Looks like you haven't added anything to your cart yet. Add something to get started."
+    >
+      <md-button class="md-raised md-primary" @click="$router.push('/shop')">
+        Browse Items
+      </md-button>
+    </md-empty-state>
+    <md-table
+      v-if="getItems.length > 0"
+      v-model="getItems"
+      md-card
+      @md-selected="onSelect"
+      class="md-elevation-0"
+    >
+      <md-table-toolbar>
+        <div class="md-toolbar-section-start">
+          <h1 class="title">Your Cart</h1>
+        </div>
+      </md-table-toolbar>
+      <md-table-toolbar slot="md-table-alternate-header" slot-scope="{ count }">
+        <div class="md-toolbar-section-start">
+          {{ getAlternateLabel(count) }}
+        </div>
+        <div class="md-toolbar-section-end">
+          <md-button class="md-icon-button" @click="removeFromCart">
+            <md-icon>delete</md-icon>
+          </md-button>
+        </div>
+      </md-table-toolbar>
+      <md-table-row
+        slot="md-table-row"
+        slot-scope="{ item }"
+        md-selectable="multiple"
+      >
+        <md-table-cell md-label="">
+          <img class="item-img" :src="item.image" :alt="item.title" />
+        </md-table-cell>
+        <md-table-cell md-label="Item">{{ item.title }}</md-table-cell>
+        <md-table-cell md-label="Category">{{ item.category }}</md-table-cell>
+        <md-table-cell md-label="Price">{{ item.price }}</md-table-cell>
+        <md-table-cell md-label="Quantity">
+          <div class="actions-container">
+            <md-button
+              v-if="item.quantity > 1"
+              class="md-icon-button"
+              @click="decreaseQuantity(item)"
+            >
+              <md-icon class="action-icon">remove</md-icon>
+            </md-button>
+            {{ item.quantity }}
+            <md-button class="md-icon-button" @click="increaseQuantity(item)">
+              <md-icon class="action-icon">add</md-icon>
+            </md-button>
+          </div>
+        </md-table-cell>
+      </md-table-row>
+    </md-table>
+    <div v-if="getItems.length > 0" class="total">
+      <h2>Total: {{ getTotal }}</h2>
+      <router-link
+        id="checkoutBtn"
+        class="md-primary"
+        to="/checkout"
+        tag="md-button"
+      >
+        Checkout
+      </router-link>
+    </div>
+    <md-dialog
+      id="checkoutDialog"
+      :md-active.sync="showDialog"
+      :md-close-on-esc="false"
+      :md-click-outside-to-close="false"
+    >
+      <h1>Processing your order</h1>
+      <md-progress-spinner
+        :md-stroke="3"
+        md-mode="indeterminate"
+      ></md-progress-spinner>
+    </md-dialog>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import axios from "axios";
+
+var formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "CAD",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
 
 export default {
-    name: "Cart",
-    computed: {
-    ...mapGetters({
-      products: 'cartProducts'
-    }),
-    total () {
-      return this.products.reduce((total, p) => {
-        return total + p.price * p.quantity
-      }, 0)
-    }
+  name: "Cart",
+  data() {
+    return {
+      items: [],
+      selected: [],
+      sending: false,
+      showDialog: false,
+    };
+  },
+  computed: {
+    getItems() {
+      return this.$store.getters["cart/getCartProducts"];
+    },
+    getTotal() {
+      return formatter.format(this.$store.getters["cart/getCartTotalPrice"]);
+    },
+    isSelected() {
+      if (this.selected.length > 0) {
+        return 1;
+      } else {
+        return 2;
+      }
+    },
   },
   methods: {
-    checkout(){
-      alert('Pay us $' + this.total)
-    }
-  }
-}
+    checkout() {
+      this.showDialog = true;
+      axios.get("/sanctum/csrf-cookie").then(() => {
+        axios
+          .post("/api/checkout", {
+            items: this.$store.getters["cart/getCartProducts"],
+          })
+          .then((response) => {
+            this.$store.dispatch("cart/checkout");
+            this.showDialog = false;
+          })
+          .catch((error) => {
+            this.showDialog = false;
+            console.log(error);
+          });
+      });
+    },
+    onSelect(items) {
+      this.selected = items;
+    },
+    getAlternateLabel(count) {
+      let plural = "";
+
+      if (count > 1) {
+        plural = "s";
+      }
+      return `${count} item${plural} selected`;
+    },
+    removeFromCart() {
+      this.selected.forEach((item) => {
+        this.$store.dispatch("cart/removeProductFromCart", item);
+      });
+    },
+    increaseQuantity(item) {
+      this.$store.dispatch("cart/incrementItemQuantity", item);
+    },
+    decreaseQuantity(item) {
+      this.$store.dispatch("cart/decrementItemQuantity", item);
+    },
+  },
+};
 </script>
 
 <style>
-body{
-    background: #f0f8ff !important;
-}
-table{
-    font-size: 20px;
-}
-.cart-message,
-h1.title {
-    text-align: center;
-    padding: 10px;
-}
-    .md-card {
-  text-align: center;
+#checkoutDialog :first-child {
+  border-radius: 24px;
+  padding: 10px;
 }
 
-#login-color {
+.md-progress-spinner {
+  justify-content: space-evenly;
+  margin: 24px;
+}
+.total {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+
+h2 {
+  padding: 20px;
+}
+
+.md-empty-state-icon {
+  color: rgb(13, 24, 48) !important;
+}
+
+.actions-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.action-icon {
+  font-size: 15px !important;
+}
+
+.md-icon-button {
+  z-index: 3;
+}
+
+.md-button + .md-button {
+  margin-left: 8px;
+}
+
+img {
+  display: block;
+  max-width: 100px;
+  max-height: 100px;
+  width: auto;
+  height: auto;
+  margin: 0 auto;
+}
+
+#checkoutBtn {
   background-color: #1d4fd8;
   background-size: 0% 100%;
   border: none;
@@ -94,9 +228,9 @@ h1.title {
   text-decoration: none;
   transition: background-color 0.5s;
   width: 150px;
+  right: 10px;
 }
-
-#login-color:hover {
+#checkoutBtn:hover {
   background-color: #011f4b;
   background-image: linear-gradient(
     to right,
@@ -108,14 +242,8 @@ h1.title {
   background-size: 200% 100%;
   transition: background-size 1s, background-color 1s;
 }
-
-#login-color:active {
+#checkoutBtn:active {
   background-color: #294fb9;
   transition: background-size 0;
 }
-
-#login-color {
-  color: white;
-}
 </style>
-
